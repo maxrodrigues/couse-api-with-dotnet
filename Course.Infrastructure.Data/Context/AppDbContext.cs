33b3db3a441e9e99bf36221bef3391c22c43a533
Maxuel Rodrigues <maxuel.rodrigues@sefaz.es.gov.br>
2026-06-10 15:23:49 -0300

    add db context and install entity framework

---
+++ Course.Infrastructure.Data/Context/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Course.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Course.Infrastructure.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Domain.Entities.Course> Courses { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+        public DbSet<Module> Modules { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
