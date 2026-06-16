1134d76293f4bf3df8b4fe6520955f72a59b0a39
Maxuel Rodrigues <maxuel.rodrigues@sefaz.es.gov.br>
2026-06-16 16:36:26 -0300

    Revert "update gitignore"
    This reverts commit 614884dc5fa76112b710cb7aad3aa799e98ace7d.

--- Course.Infrastructure.Data/Repositories/CategoryRepository.cs
+++ Course.Infrastructure.Data/Repositories/CategoryRepository.cs
@@ -1,43 +1,33 @@
 ﻿using Course.Domain.Entities;
 using Course.Domain.Interfaces;
-using Course.Infrastructure.Data.Context;
-using Microsoft.EntityFrameworkCore;
 
 namespace Course.Infrastructure.Data.Repositories
 {
     public class CategoryRepository : ICategoryRepository
     {
-        private readonly AppDbContext _context;
-        public async Task<Category> GetByIdAsync(int id)
+        public Task<Category> GetByIdAsync(int id)
         {
-            return await _context.Categories.Where(x => x.DeletedAt != null && x.Id == id).FirstOrDefaultAsync();
+            throw new NotImplementedException();
         }
 
-        public async Task<List<Category>> GetAllAsync()
+        public Task<List<Category>> GetAllAsync()
         {
-            return await _context.Categories.Where(x => x.DeletedAt != null).ToListAsync();
+            throw new NotImplementedException();
         }
 
-        public async Task AddAsync(Category category)
+        public Task AddAsync(Category category)
         {
-            _context.Categories.Add(category);
-            await _context.SaveChangesAsync();
+            throw new NotImplementedException();
         }
 
-        public async Task UpdateAsync(Category category)
+        public Task UpdateAsync(int id, Category category)
         {
-            _context.Categories.Update(category);
-            await _context.SaveChangesAsync();
+            throw new NotImplementedException();
         }
 
-        public async Task DeleteAsync(int id)
+        public Task DeleteAsync(int id)
         {
-            var category = await this.GetByIdAsync(id);
-            if (category != null)
-            {
-                category.DeletedAt = DateTime.UtcNow;
-                await _context.SaveChangesAsync();
-            }
+            throw new NotImplementedException();
         }
     }
 }
