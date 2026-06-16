317a7c82d304ee6b2a0e6f1cc9ace8c2dd5fcee7
Maxuel Rodrigues <maxuel.rodrigues@sefaz.es.gov.br>
2026-06-16 16:29:15 -0300

    add database and scope interfaces and repositories

---
+++ Course.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Course.Domain.Interfaces;
+using Course.Infrastructure.Data.Context;
+using Course.Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Course.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlite(configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName));
+            });
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ICourseRepository, CourseRepository>();
+            services.AddScoped<ILessonRepository, LessonRepository>();
+            services.AddScoped<IModuleRepository, ModuleRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            
+            return services;
+        }
+    }
+}
